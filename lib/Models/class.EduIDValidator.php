52020ced0ef33599cf66063ed639e031bb141c13
Christian Glahn <cpglahn@gmail.com>
2016-05-14 16:49:40 +0200

    added convenience functions to all validator classes

--- lib/Models/class.EduIDValidator.php
+++ lib/Models/class.EduIDValidator.php
@@ -10,8 +10,34 @@ class EduIDValidator extends RESTlingValidator {
     protected $id_type;     // service, client, user
     protected $uuid;        // respective uuid
 
+    protected $allowEmptyMethod = array();
+    protected $requireEmptyMethod = array();
+
     public function __construct($db) {
         $this->db = $db;
+    }
+
+    public function allowEmpty($methodList) {
+        if (isset($methodList)) {
+            if (!is_array($methodList)) {
+                $methodLis = array($methodList);
+            }
+            foreach ($methodList as $m) {
+                $m = strtolower($m);
+                $this->allowEmptyMethod[] = $m;
+            }
+        }
+    }
+    public function requireEmpty($methodList) {
+        if (isset($methodList)) {
+            if (!is_array($methodList)) {
+                $methodLis = array($methodList);
+            }
+            foreach ($methodList as $m) {
+                $m = strtolower($m);
+                $this->requireEmptyMethod[] = $m;
+            }
+        }
     }
 }
 
