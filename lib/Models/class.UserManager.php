c00678ff799ec4ae395a680d2580e113351cd7e0
Christian Glahn <cpglahn@gmail.com>
2016-05-14 17:38:12 +0200

    Access personal profiles via user-profile service

--- lib/Models/class.UserManager.php
+++ lib/Models/class.UserManager.php
@@ -7,6 +7,7 @@ require_once("Models/class.DBManager.php");
 
 class UserManager extends DBManager{
     private $user;
+    private $profile;
 
     public function findByMailAddress($mailAddress) {
         $this->user = null;
@@ -69,11 +70,47 @@ class UserManager extends DBManager{
         return false;
     }
 
+    public function loadProfile() {
+        $this->profile = null;
+
+        if (isset($this->user) &&
+            !empty($this->user) &&
+            !empty($this->user["user_uuid"])) {
+
+            $aFields = array("idp_uuid", "userID", "mailAddress", "invalid", "extra");
+            $sqlstr = "select " . implode($aFields) . " from useridentities where user_uuid = ?";
+            $sth = $this->db->prepare($sqlstr, array("TEXT"));
+            $res = $sth->execute(array($this->user["user_uuid"]));
+
+            $this->profile = array();
+
+            while ($row = $res->fetchRow(MDB2_FETCHMODE_ASSOC)) {
+                foreach ($aFields as $f) {
+                    $aProfiles = array();
+                    if (isset($row[$f]) && !empty($row[$f])) {
+                        if ($f == "extra") {
+                            $aProfiles[$f] = json_decode($row[$f]);
+                        }
+                        else {
+                            $aProfiles[$f] = $row[$f];
+                        }
+                    }
+                }
+            }
+
+            $sth->free();
+        }
+    }
+
     public function getUUID() {
         if (isset($this->user) && !empty($this->user)) {
             return $this->user["user_uuid"];
         }
         return null;
+    }
+
+    public function getAllProfiles() {
+        return $this->profile;
     }
 }
 
--- lib/Service/class.user-profile.php
+++ lib/Service/class.user-profile.php
@@ -3,4 +3,22 @@
  *
  * *********************************************************************** */
 
+class UserAuthService extends ServiceFoundation {
+    protected function initializeRun() {
+        $this->tokenValidator->resetAcceptedTokens(array("Bearer", "MAC"));
+        $this->tokenValidator->setAcceptedTokenTypes(array("Bearer", "MAC"));
+        $this->tokenValidator->requireUser();
+        $this->tokenValidator->requireClient();
+    }
+
+    protected function get() {
+        if ($user = $this->tokenValidator->getTokenUser()) {
+            $this->data = $user->getAllProfiles();
+        }
+        else {
+            $this->forbidden();
+        }
+    }
+}
+
 ?>
