5cf685a8f595bf2abe559a687245ee8859ce3638
Christian Glahn <cpglahn@gmail.com>
2016-05-30 15:34:01 +0200

    add JWT submodule

--- lib/Service/class.AssertionService.php
+++ lib/Service/class.AssertionService.php
@@ -8,6 +8,15 @@
  */
 class AssertionService extends ServiceFoundation {
 
+   /**
+    *
+    */
+    public function __construct() {
+        parent::__construct();
+        $this->tokenValidator->resetAcceptedTokens(array("Bearer", "MAC"));
+    }
+
+
 }
 
 ?>
