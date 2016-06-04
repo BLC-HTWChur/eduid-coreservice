c3a2a344d0741e8b3ab7026c34a89ffaada0fee2
Christian Glahn <phish@Christians-Mac-mini.local>
2016-06-04 18:10:14 +0200

    test functions

---
+++ tests/remote/basic.php
@@ -0,0 +1,88 @@
+<?php
+
+set_include_path("../../lib" . PATH_SEPARATOR .
+                get_include_path());
+
+// load RESTling
+include_once('RESTling/contrib/Restling.auto.php');
+include_once('eduid.autoloader.php');
+
+require('./class.curler.php');
+
+$taddr = "192.168.0.72";
+$tpath = "/eduid/eduid.php";
+$tinfo = "token";
+
+$c = new Curler(array("host" => $taddr, "path"=> $tpath));
+
+$c->get();
+
+echo ($c->getStatus() == 403 ? "OK": "FAILED ");
+echo "\n";
+
+$c->setPathInfo($tinfo);
+$c->get();
+
+echo ($c->getStatus() == 400 ? "OK": "FAILED");
+echo "\n";
+
+use Lcobucci\JWT as JWT;
+use Lcobucci\JWT\Signer as Signer;
+
+$jwt = new JWT\Builder();
+
+$access_key = "acf5acfaa58665e6e74f9d03e504b7dce7bc9568";
+$mac_key    = "helloWorld";
+$kid        = '1234test-12';
+
+$jwt->setIssuer('io.mobinaut.test');
+$jwt->setAudience("http://192.168.0.72/eduid/eduid.php/token");
+
+$jwt->setHeader("kid", $kid);
+// $jwt->setId('123asd3414fafr23r');
+        
+$jwt->setSubject("phish@lo-f.at"); // use my email as device id 
+$jwt->set("name", "phishs tester"); // use fake name
+
+$cn = 'Lcobucci\JWT\Signer\Hmac\Sha256';
+$signer = new $cn;
+$jwt->sign($signer,
+           $mac_key);
+
+$t = $jwt->getToken();
+
+$c->setHeader(array("Authorization"=> "Bearer $t"));
+
+$d = array("grant_type" => "client_credentials");
+$c->post(json_encode($d), "application/json");
+
+
+echo ($c->getStatus() == 200 ? "OK": "FAILED");
+echo "\n";
+
+$clientToken = json_decode($c->getBody(), true);
+
+$c->resetHeader();
+$c->setMacToken($clientToken);
+// log in the user
+
+// user info
+$userpw = "helloworld";
+
+$data = array("grant_type"=> "password", "username"=> "christian.glahn@htwchur.ch", "password" => $userpw);
+
+$jsonData = json_encode($data);
+$c->post($jsonData, "application/json");
+
+echo ($c->getStatus() == 200 ? "OK": "FAILED");
+echo "\n";
+
+if ($c->getStatus() == 200) {
+    $userToken = json_decode($c->getBody(), true);
+    $c->setMacToken($userToken); // now we are logged in
+}
+
+
+
+
+?>
---
+++ tests/remote/class.curler.php
@@ -0,0 +1,221 @@
+<?php 
+
+class Curler {
+    private $curl;
+    private $protocol;
+    private $host;
+    private $base_url;
+    private $path_info;
+    
+    private $param;
+    private $body;
+    
+    private $status;
+    private $out_header;
+    private $in_header;
+    private $mac_token;
+    
+    private $next_url;
+    private $next_method;
+    
+    public function __construct($options) {
+        
+        $this->protocol   = array_key_exists("protocol", $options) ? $options["protocol"] : "http";
+        $this->host       = array_key_exists("host", $options) ? $options["host"] : "";
+        $this->base_url   = array_key_exists("path", $options) ? $options["path"] : "";
+        $this->path_info  = array_key_exists("path_info", $options) ? $options["path_info"] : "";
+        $this->param = array();
+        $this->out_header = array();
+    }
+    
+    public function setPathInfo($pi) {
+        $this->path_info = $pi;
+    }
+    
+    public function setGetParameter($p) {
+        $this->param = $p;
+    }
+    
+    public function setHeader($p) {
+        $this->out_header = $p;
+    }
+    
+    public function resetHeader() {
+        $this->out_header = null;
+    }
+    
+    public function setMacToken($t) {
+        $this->mac_token = $t;
+    }
+    
+    private function prepareUri() {
+        $this->path_info = ltrim($this->path_info, "/");
+        $this->next_url  = $this->protocol . "://" . $this->host . $this->base_url . "/" . $this->path_info;
+            
+        if (!empty($this->param)) {
+            $tp = array();
+            foreach ($this->param as $k => $v) {
+                array_push($tp, urlencode($k) . "=" . urlencode($v));
+            }
+            if (!empty($tp)) {
+                $this->next_url .= "?". implode("&", $tp);
+            }
+        }
+    }
+    
+    private function getSigner($alg) {
+        $signer = null;
+        
+        list($algo, $level) = explode("S", $alg);
+
+        switch ($algo) {
+            case "H": $algo = "Hmac"; break;
+            case "R": $algo = "Rsa"; break;
+            case "E": $algo = "Ecdsa"; break;
+            default: $algo = ""; break;
+        }
+        switch ($level) {
+            case "256":
+            case "384":
+            case "512":
+                break;
+            default: $level = ""; break;
+        }
+
+        if (!empty($algo) && !empty($level)) {
+            // NOTE: for dynamic namespaced classes the fully qualified name is needed.
+            $signerClass = "Lcobucci\\JWT\\Signer\\" . $algo . "\\Sha" . $level ;
+            $signer = new $signerClass();
+        }
+        
+        return $signer;
+    }
+    
+    private function prepareOutHeader($type="") {
+        
+        $th = array();
+            
+        if (!empty($type)) {
+            array_push($th, "Content-Type: " . $type);
+        }
+            
+        if (isset($this->mac_token) && !empty($this->mac_token)) {
+            // sign mac token
+            $signer = $this->getSigner($this->mac_token["mac_algorithm"]);
+                
+            // generate payload
+            $ts = time();
+            
+            $payload = $this->next_method . " " . $this->base_url . "/" . $this->path_info . " HTTP/1.1\n";
+            $payload .= "$ts\n";
+            $payload .= $this->host . "\n";
+                
+            $mac = base64_encode($signer->sign($payload, $this->mac_token["mac_key"]));
+            array_push($th, "Authorization: MAC kid=". $this->mac_token["kid"] . ",ts=$ts,mac=" . $mac);
+        }
+        
+        if (!empty($this->out_header)) {
+            foreach ($this->out_header as $k => $v) {
+                array_push($th, $k . ": " . $v);
+            }
+        }
+        if (!empty($th)) {
+            curl_setopt($this->curl, CURLOPT_HTTPHEADER, $th);
+        }
+    }
+    
+    private function request() {
+        $res = curl_exec($this->curl);
+        
+        $this->in_header = array();
+        
+        $this->in_header["content_type"] = curl_getinfo($this->curl, CURLINFO_CONTENT_TYPE);
+        $this->status = curl_getinfo($this->curl, CURLINFO_RESPONSE_CODE);
+        
+        $this->body = $res;
+        
+        curl_close($this->curl);
+    }
+    
+    public function get() {
+        $this->next_method = "GET";
+        $this->prepareUri();
+        $c = curl_init($this->next_url);
+        
+        $this->curl = $c;
+        
+        // curl_setopt($c, CURLOPT_HEADER, true);
+        curl_setopt($c, CURLOPT_RETURNTRANSFER, true);   
+        curl_setopt($c, CURLOPT_CUSTOMREQUEST, $this->next_method );
+        $this->prepareOutHeader();
+        
+        $this->request();
+    }
+    
+    public function post($data, $type) {
+        $this->next_method = "POST";
+        $this->prepareUri();
+        $c = curl_init($this->next_url);
+        
+        $this->curl = $c;
+        
+        // curl_setopt($c, CURLOPT_HEADER, true);
+        curl_setopt($c, CURLOPT_RETURNTRANSFER, true);   
+        curl_setopt($c, CURLOPT_CUSTOMREQUEST, $this->next_method );
+        
+        $this->prepareOutHeader($type);
+        
+        curl_setopt($c, CURLOPT_POSTFIELDS, $data); 
+        
+        $this->request();
+        
+    }
+    
+    public function put($data, $type) {
+        $this->next_method = "PUT";
+        $this->prepareUri();
+        $c = curl_init($this->next_url);
+        
+        $this->curl = $c;
+        
+        // curl_setopt($c, CURLOPT_HEADER, true);
+        curl_setopt($c, CURLOPT_RETURNTRANSFER, true);   
+        curl_setopt($c, CURLOPT_CUSTOMREQUEST, $this->next_method );
+        
+        $this->prepareOutHeader($type);
+        
+        curl_setopt($c, CURLOPT_POSTFIELDS, $data);        
+        
+        $this->request();
+    }
+        
+    
+    public function delete(){
+        $this->next_method = "DELETE";
+        $this->prepareUri();
+        $c = curl_init($this->next_url);
+        
+        $this->curl = $c;
+        
+        // curl_setopt($c, CURLOPT_HEADER, true);
+        curl_setopt($c, CURLOPT_RETURNTRANSFER, true);   
+        curl_setopt($c, CURLOPT_CUSTOMREQUEST, $this->next_method );
+        
+        $this->prepareOutHeader();     
+        
+        $this->request();
+    }
+    
+    public function getStatus() {
+        return $this->status;
+    }
+    
+    public function getHeader() {
+        return $this->in_header;
+    }
+    
+    public function getBody() {
+        return $this->body;
+    }
+}
+?>
