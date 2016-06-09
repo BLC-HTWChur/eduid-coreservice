5501f2c187aa178373a2754fb8dd263d656a090f
Christian Glahn <cpglahn@gmail.com>
2016-06-09 07:52:25 +0200

    typo fix and comments

--- lib/eduid.auto.php
+++ lib/eduid.auto.php
@@ -8,10 +8,18 @@ spl_autoload_register(function ($class) {
 
     if (isset($root) && !empty($root)) {
         $cpath = array();
+        // direct namespace
         $cpath[] = $root . "/" . implode("/", $parts) . ".class.php";
-        $cpath[] = $root . "/classes/" . . implode("/", $parts) . ".class.php";
+
+        // sub-directory namespaces
+        $cpath[] = $root . "/classes/" . implode("/", $parts) . ".class.php";
+        $cpath[] = $root . "/src/" . implode("/", $parts) . ".class.php";
+        $cpath[] = $root . "/lib/" . implode("/", $parts) . ".class.php";
+
+        // for developer prefixed namespaces
         $root = array_shift($parts);
-        $cpath[] = strtolower($root) . "/src/" . . implode("/", $parts) . ".class.php";
+        $cpath[] = strtolower($root) . "/src/" . implode("/", $parts) . ".class.php";
+        $cpath[] = strtolower($root) . "/lib/" . implode("/", $parts) . ".class.php";
 
         $prefixes = explode(PATH_SEPARATOR, get_include_path());
 
